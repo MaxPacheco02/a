c1201019da646efbd9c4115e5c3d6628551ccece
MaxPacheco02 <a01552369@tec.mx>
2024-11-01 00:22:23 -0600

    changes

--- lidar_conf_usv.sh
+++ lidar_conf_usv.sh
@@ -4,4 +4,4 @@ sudo route del 192.168.1.201
 sudo ifconfig enp108s0 192.168.1.200
 sudo route add 192.168.1.201 enp108s0
 
-# IPV4 should be set manually to 192.168.1.101 / 24
+# IPV4 should be set manually to 192.168.1.201 / 24
